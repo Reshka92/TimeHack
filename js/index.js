ac46f411265c33fa59d2ecbb6e1faff2e5a3e24e
Мартынов Кирилл <kirillmatveev119@gmail.com>
2025-12-19 15:02:39 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-//da
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('addBtn').addEventListener('click', function(){
+        document.getElementById('addMenu').classList.toggle('hidden');
+    });
+});
